7d3a8d54357048688d8064ee761b782d0b5589dd
Roger Ehmpke <rehmpke@gmail.com>
2017-12-04 23:30:14 -0600

    Added lazy loading img JS function as well as css for them

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,14 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
+// Lazy load images
+// ex. <img data-src="/path/to/image.jpg" alt="">
+(function() {
+  [].forEach.call(document.querySelectorAll('img[data-src]'), function(img) {
+    img.setAttribute('src', img.getAttribute('data-src'));
+    img.onload = function() {
+      img.removeAttribute('data-src');
+    };
+  });
+})();
